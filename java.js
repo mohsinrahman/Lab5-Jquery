ce6198c5008e67eca32956511de611588e8f5191
AleksandarP91 <Pejcinovski91@gmail.com>
2015-11-11 12:52:14 +0100

    focus blur continue from lab4

---
+++ java.js
@@ -0,0 +1,8 @@
+﻿
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null  || x == "") {
+        document.getElementById("return").innerHTML="Please enter a username";
+        return false;
+    }
+}
